aeae3c6c1cff7656c4a54accef6075fb71c71c06
Victor Martins <victor.martins1@aluno.ufop.edu.br>
2023-08-13 19:55:50 -0300

    refactor: [str] changed to List[str]

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI, BackgroundTasks, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from typing import Union
+from typing import Union, List
 
 app = FastAPI()
 
@@ -24,7 +24,7 @@ class SuperSurveySave(BaseModel):
     platform: str
     city: str
     user_id: str
-    columns: [str]
+    columns: List[str]
     clusterization_method: Union[str, None] = None
     aggregation_method: Union[str, None] = None
     start_date: Union[str, None] = None
@@ -34,22 +34,17 @@ class SuperSurveySave(BaseModel):
 
 
 @app.post('/super_survey/save')
-# @cross_origin(data)
 async def save(data: SuperSurveySave):
     return super_survey.save(data)
 
 
 @app.post('/super_survey/continue')
-# @cross_origin(data)
 async def restart(data):
-    # data = request.get_json(data)
     return super_survey.restart(data)
 
 
 @app.post('/super_survey/update')
-# @cross_origin(data)
 async def update(data):
-    # data = request.get_json(data)
     return super_survey.update(data)
 
 
@@ -59,9 +54,7 @@ async def get_data_columns(platform: str):
 
 
 @app.post('/nav/export')
-# @cross_origin(data)
 async def export_super_survey(data):
-    # data = request.get_json(data)
     return nav.export(data)
 
 
@@ -71,111 +64,82 @@ class NavList(BaseModel):
 
 @app.post('/nav/list')
 async def export_super_survey_info(data: NavList):
-    # data = request.get_json(data)
     return nav.list(data)
 
 
 @app.post('/nav/public_getall')
-# @cross_origin(data)
 async def export_public_super_survey_info():
     return nav.public_getall()
 
 
 @app.post('/nav/getbycity')
-# @cross_origin(data)
 async def getbycity(data):
-    # data = request.get_json(data)
     return nav.getbycity(data)
 
 
 @app.post('/nav/getbyid')
-# @cross_origin(data)
 async def getbyid(data):
-    # data = request.get_json(data)
     return super_survey.getbyid(data)
 
 
 @app.post('/nav/prepare')
-# @cross_origin(data)
 async def prepare(data):
-    # data = request.get_json(data)
     return super_survey.prepare(data)
 
 
 @app.get('/nav/preparefilter/{ss_id}')
-# @cross_origin(data)
 async def prepare_filter(ss_id: str):
     # args = request.args
     return super_survey.prepare_filter(ss_id)
 
 
 @app.post('/nav/chart')
-# @cross_origin(data)
 async def chart(data):
-    # data = request.get_json(data)
     return super_survey.chart(data)
 
 
 @app.post('/auth/login')
-# @cross_origin(data)
 async def login(data):
-    # data = request.get_json(data)
     return auth.login(data)
 
 
 @app.post('/auth/register')
-# @cross_origin(data)
 async def register(data):
-    # data = request.get_json(data)
     return auth.register(data)
 
 
 @app.post('/auth/edit_user')
-# @cross_origin(data)
 async def edit_user(data):
-    # data = request.get_json(data)
     return auth.edit_user(data)
 
 
 @app.post('/auth/change_password')
-# @cross_origin(data)
 async def change_password(data):
-    # data = request.get_json(data)
     return auth.change_password(data)
 
 
 @app.post('/auth/forgot_password')
-# @cross_origin(data)
 async def forgot_password(data):
-    # data = request.get_json(data)
     return auth.forgot_password(data)
 
 
 @app.post('/users/list')
-# @cross_origin(data)
 async def list(data):
-    # data = request.get_json(data)
     return users.list(data)
 
 
 @app.post('/users/change_permission')
-# @cross_origin(data)
 async def change_permission(data):
-    # data = request.get_json(data)
     return users.change_permission(data)
 
 
 @app.post('/users/delete')
-# @cross_origin(data)
 async def delete_user(data):
-    # data = request.get_json(data)
     return users.delete(data)
 
 
 @app.post('/users/accept')
-# @cross_origin(data)
 async def accept(data):
-    # data = request.get_json(data)
     return users.accept(data)
 
 
