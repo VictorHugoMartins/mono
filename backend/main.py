48ce1a88db6b60aac7dcf58ef059371b0722bfe0
Victor Martins <victor.martins1@aluno.ufop.edu.br>
2023-08-13 18:47:28 -0300

    refactor: change flask to fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,171 +1,169 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+import controllers.nav as nav
+import controllers.users as users
+import controllers.auth as auth
+import controllers.super_survey as super_survey
+from fastapi import FastAPI, BackgroundTasks, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS, cross_origin
 
 # controllers
-import controllers.super_survey as super_survey
-import controllers.auth as auth
-import controllers.users as users
-import controllers.nav as nav
 
-app = Flask(__name__)  # Dados de usuário armazenados em um dicionário
-CORS(app)
+# app = Flask(__name__)  # Dados de usuário armazenados em um dicionário
+# CORS(app)
 
 
-@app.route('/super_survey/save', methods=['POST'])
-@cross_origin()
-def save():
-    data = request.get_json()
+@app.post('/super_survey/save')
+# @cross_origin(data)
+async def save(data):
+    # data = request.get_json(data)
     return super_survey.save(data)
 
 
-@app.route('/super_survey/continue', methods=['POST'])
-@cross_origin()
-def restart():
-    data = request.get_json()
+@app.post('/super_survey/continue')
+# @cross_origin(data)
+async def restart(data):
+    # data = request.get_json(data)
     return super_survey.restart(data)
 
-@app.route('/super_survey/update', methods=['POST'])
-@cross_origin()
-def update():
-    data = request.get_json()
+
+@app.post('/super_survey/update')
+# @cross_origin(data)
+async def update(data):
+    # data = request.get_json(data)
     return super_survey.update(data)
 
 
-@app.route('/super_survey/get_data_columns', methods=['GET'])
-@cross_origin()
-def get_data_columns():
-    args = request.args
-    return super_survey.get_data_columns(args)
+@app.get('/super_survey/get_data_columns')
+# @cross_origin(data)
+async def get_data_columns(data):
+    # args = request.args
+    return super_survey.get_data_columns(data)
 
 
-@app.route('/nav/export', methods=['POST'])
-@cross_origin()
-def export_super_survey():
-    data = request.get_json()
+@app.post('/nav/export')
+# @cross_origin(data)
+async def export_super_survey(data):
+    # data = request.get_json(data)
     return nav.export(data)
 
 
-@app.route('/nav/list', methods=['POST'])
-@cross_origin()
-def export_super_survey_info():
-    data = request.get_json()
+@app.post('/nav/list')
+# @cross_origin(data)
+async def export_super_survey_info(data):
+    # data = request.get_json(data)
     return nav.list(data)
 
 
-@app.route('/nav/public_getall', methods=['POST'])
-@cross_origin()
-def export_public_super_survey_info():
-    data = request.get_json()
+@app.post('/nav/public_getall')
+# @cross_origin(data)
+async def export_public_super_survey_info(data):
+    data = request.get_json(data)
     print(data)
     return nav.public_getall(data)
 
 
-@app.route('/nav/getbycity', methods=['POST'])
-@cross_origin()
-def getbycity():
-    data = request.get_json()
+@app.post('/nav/getbycity')
+# @cross_origin(data)
+async def getbycity(data):
+    # data = request.get_json(data)
     return nav.getbycity(data)
 
 
-@app.route('/nav/getbyid', methods=['POST'])
-@cross_origin()
-def getbyid():
-    data = request.get_json()
+@app.post('/nav/getbyid')
+# @cross_origin(data)
+async def getbyid(data):
+    # data = request.get_json(data)
     return super_survey.getbyid(data)
 
 
-@app.route('/nav/prepare', methods=['POST'])
-@cross_origin()
-def prepare():
-    data = request.get_json()
+@app.post('/nav/prepare')
+# @cross_origin(data)
+async def prepare(data):
+    # data = request.get_json(data)
     return super_survey.prepare(data)
 
 
-@app.route('/nav/preparefilter', methods=['GET'])
-@cross_origin()
-def prepare_filter():
-    args = request.args
-    return super_survey.prepare_filter(args)
+@app.get('/nav/preparefilter')
+# @cross_origin(data)
+async def prepare_filter(data):
+    # args = request.args
+    return super_survey.prepare_filter(data)
 
 
-@app.route('/nav/chart', methods=['POST'])
-@cross_origin()
-def chart():
-    data = request.get_json()
+@app.post('/nav/chart')
+# @cross_origin(data)
+async def chart(data):
+    # data = request.get_json(data)
     return super_survey.chart(data)
 
 
-@app.route('/auth/login', methods=['POST'])
-@cross_origin()
-def login():
-    data = request.get_json()
+@app.post('/auth/login')
+# @cross_origin(data)
+async def login(data):
+    # data = request.get_json(data)
     return auth.login(data)
 
 
-@app.route('/auth/register', methods=['POST'])
-@cross_origin()
-def register():
-    data = request.get_json()
+@app.post('/auth/register')
+# @cross_origin(data)
+async def register(data):
+    # data = request.get_json(data)
     return auth.register(data)
 
 
-@app.route('/auth/edit_user', methods=['POST'])
-@cross_origin()
-def edit_user():
-    data = request.get_json()
+@app.post('/auth/edit_user')
+# @cross_origin(data)
+async def edit_user(data):
+    # data = request.get_json(data)
     return auth.edit_user(data)
 
 
-@app.route('/auth/change_password', methods=['POST'])
-@cross_origin()
-def change_password():
-    data = request.get_json()
+@app.post('/auth/change_password')
+# @cross_origin(data)
+async def change_password(data):
+    # data = request.get_json(data)
     return auth.change_password(data)
 
 
-@app.route('/auth/forgot_password', methods=['POST'])
-@cross_origin()
-def forgot_password():
-    data = request.get_json()
+@app.post('/auth/forgot_password')
+# @cross_origin(data)
+async def forgot_password(data):
+    # data = request.get_json(data)
     return auth.forgot_password(data)
 
 
-@app.route('/users/list', methods=['POST'])
-@cross_origin()
-def list():
-    data = request.get_json()
+@app.post('/users/list')
+# @cross_origin(data)
+async def list(data):
+    # data = request.get_json(data)
     return users.list(data)
 
 
-@app.route('/users/change_permission', methods=['POST'])
-@cross_origin()
-def change_permission():
-    data = request.get_json()
+@app.post('/users/change_permission')
+# @cross_origin(data)
+async def change_permission(data):
+    # data = request.get_json(data)
     return users.change_permission(data)
 
 
-@app.route('/users/delete', methods=['POST'])
-@cross_origin()
-def delete_user():
-    data = request.get_json()
+@app.post('/users/delete')
+# @cross_origin(data)
+async def delete_user(data):
+    # data = request.get_json(data)
     return users.delete(data)
 
 
-@app.route('/users/accept', methods=['POST'])
-@cross_origin()
-def accept():
-    data = request.get_json()
+@app.post('/users/accept')
+# @cross_origin(data)
+async def accept(data):
+    # data = request.get_json(data)
     return users.accept(data)
 
 
-@app.route('/', methods=['GET'])
-@cross_origin()
-def hello_world():
-    try:
-        return "Hello World"
-    except:
-        return jsonify({"message": "Erro!", "success": False}), 500
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.get("/")
+async def root(data):
+    return {"message": "Hello World. Welcome to FastAPI!"}
