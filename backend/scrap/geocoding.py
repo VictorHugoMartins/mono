04cc39f4ceeaeb8e42b656a401ff2172191b1225
Victor Martins <victor.martins1@aluno.ufop.edu.br>
2023-08-15 19:47:09 -0300

    fix: bounding box 3

--- backend/scrap/geocoding.py
+++ backend/scrap/geocoding.py
@@ -293,19 +293,20 @@ class BoundingBox():
 
                 print(location.raw)
 
-
-
                 # 2 3 0 1
                 # 1 3 0 2
                 # 0 2 1 3
+                # 3 2
 
                 # [-20.2519, -43.4372, -20.5229, -43.7846]
                 # [n_lat, e_lng, s_lat, w_lng]
 
+                print(bounds)
+                #1 2 0 3 
                 bounding_box = (bounds[1],
-                                bounds[3],
+                                bounds[2],
                                 bounds[0],
-                                bounds[2]
+                                bounds[3]
                                 )
 
                 print(bounding_box)
