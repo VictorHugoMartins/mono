39abe894330494a219f31b30483c51dea1c925a4
Victor Martins <victor.martins1@aluno.ufop.edu.br>
2023-08-15 03:30:53 -0300

    fix: change typing format

--- backend/models/nav.py
+++ backend/models/nav.py
@@ -1,7 +1,6 @@
-from __future__ import annotations
-
 from pydantic import BaseModel
-from typing import List
+from typing import Union, List
+
 
 class ExportModel(BaseModel):
     ss_id: str
@@ -20,41 +19,41 @@ class GetByIdModel(BaseModel):
     aggregation_method: str
     clusterization_method: str
     platform: str
-    room_id: str | None = None
-    host_id: str | None = None
-    price: float | None = None
-    name: str | None = None
-    latitude: str | None = None
-    longitude: str | None = None
-    property_type: List[str] | None = None
-    room_type: List[str] | None = None
-    minstay: int | None = None
-    reviews: float | None = None
-    accommodates: int | None = None
-    bedrooms: str | None = None
-    bathrooms: str | None = None
-    bathroom: str | None = None
-    extra_host_languages: str | None = None
-    is_superhost: str | None = None
-    comodities: str | None = None
-    route: List[str] | None = None
-    sublocality: List[str] | None = None
-    locality: List[str] | None = None
-    avg_rating: float | None = None
-    location_id: List[str] | None = None
-    ss_id: str | None = None
-    city: str | None = None
-    status: str | None = None
-    logs: str | None = None
-    date: str | None = None
+    room_id: Union[str, None] = None
+    host_id: Union[str, None] = None
+    price: Union[float, None] = None
+    name: Union[str, None] = None
+    latitude: Union[str, None] = None
+    longitude: Union[str, None] = None
+    property_type: Union[List[str], None] = None
+    room_type: Union[List[str], None] = None
+    minstay: Union[int, None] = None
+    reviews: Union[float, None] = None
+    accommodates: Union[int, None] = None
+    bedrooms: Union[str, None] = None
+    bathrooms: Union[str, None] = None
+    bathroom: Union[str, None] = None
+    extra_host_languages: Union[str, None] = None
+    is_superhost: Union[str, None] = None
+    comodities: Union[str, None] = None
+    route: Union[List[str], None] = None
+    sublocality: Union[List[str], None] = None
+    locality: Union[List[str], None] = None
+    avg_rating: Union[float, None] = None
+    location_id: Union[List[str], None] = None
+    ss_id: Union[str, None] = None
+    city: Union[str, None] = None
+    status: Union[str, None] = None
+    logs: Union[str, None] = None
+    date: Union[str, None] = None
 
-    n_clusters: int | None = None
-    init: str | None = None
-    n_init: int | None = None
-    eps: float | None = None
-    min_samples: int | None = None
-    threshold: float | None = None
-    branching_factor: int | None = None
+    n_clusters: Union[int, None] = None
+    init: Union[str, None] = None
+    n_init: Union[int, None] = None
+    eps: Union[float, None] = None
+    min_samples: Union[int, None] = None
+    threshold: Union[float, None] = None
+    branching_factor: Union[int, None] = None
 
 
 class PrepareModel(BaseModel):
