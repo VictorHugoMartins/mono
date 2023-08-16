9b012602fd773c67c0bf1213104302f0cf983d6f
Victor Martins <victor.martins1@aluno.ufop.edu.br>
2023-08-15 21:44:05 -0300

    refactor: views select

--- backend/utils/general_dict.py
+++ backend/utils/general_dict.py
@@ -199,144 +199,29 @@ statusDict = {
 # SQL SCRIPTS
 
 
-def get_airbnb_rooms_by_ss_id(ss_id, aggregation_method='_avg'):
-    return """SELECT * from rooms{aggregation_method} 
-							WHERE
-								survey_id in (
-									select
-										distinct(survey_id) as survey_id
-									from
-										survey
-									where
-										ss_id = {ss_id}
-								)
-								AND platform = 'Airbnb'
-							ORDER BY locality, sublocality, route, location_id, room_id""".format(ss_id=ss_id, aggregation_method=aggregation_method)
-
-
-def get_airbnb_rooms_by_locality(locality, aggregation_method='_avg'):
-    return """SELECT * from rooms{aggregation_method} 
-							WHERE
-								locality = {locality}
-								AND platform = 'Airbnb'
-							ORDER BY locality, sublocality, route, location_id, room_id""".format(locality=locality, aggregation_method=aggregation_method)
-
-
-def get_booking_rooms_by_ss_id(ss_id, aggregation_method='_avg'):
-    return """SELECT * from rooms{aggregation_method} 
-							WHERE
-								survey_id in (
-									select
-										distinct(survey_id) as survey_id
-									from
-										survey
-									where
-										ss_id = {ss_id}
-								)
-								AND platform = 'Booking'
-							ORDER BY locality, sublocality, route, location_id, room_id""".format(ss_id=ss_id, aggregation_method=aggregation_method)
-
-
 def get_all_rooms_by_ss_id(ss_id, platform="'Airbnb' or platform = 'Booking'", aggregation_method='_avg'):
     s_ids = select_command(sql_script="""SELECT distinct(survey_id) from survey where ss_id = %s""",
                            params=(ss_id,),
                            initial_message="Selecting status from super survey",
                            failure_message="Failed to select status for super survey")
 
-    query = 'and'
+    query = 'and ('
     if (s_ids and len(s_ids) > 0):
         for s_id in s_ids:
             query = "{query}{x}".format(query=query,
-                x=" (strpos(test.surve_id, '{s},') <> 0) or (strpos(test.surve_id, ',{s}') <> 0) or test.surve_id = '{s}' or".format(s=s_id[0]))
+                                        x=" (strpos(accommodates{ag}.surve_id, '{s},') <> 0) or (strpos(accommodates{ag}.surve_id, ',{s}') <> 0) or test.surve_id = '{s}' or".format(a=aggregation_method, s=s_id[0]))
     query = removeLastWordOfString('or', query)
     query = removeLastWordOfString('and', query)
+    query = "{query})".format(query=query)
 
-    return """SELECT * from test
+    return """SELECT * from accommodates{aggregation_method}
 							  WHERE platform = {platform} {query}
 							ORDER BY locality, sublocality, route, location_id, room_id""".format(ss_id=ss_id, query=query, platform=platform, aggregation_method=aggregation_method)
 
 
 def get_all_rooms_by_locality(locality, platform="'Airbnb' or platform = 'Booking'", aggregation_method='_avg'):
-    return """SELECT * from rooms{aggregation_method} 
+    return """SELECT * from accommodates{aggregation_method} 
 							WHERE
 								locality = {locality}
 								AND platform = {platform}
 							ORDER BY locality, sublocality, route, location_id, room_id""".format(locality=locality, platform=platform, aggregation_method=aggregation_method)
-
-
-def deprecated_get_airbnb_rooms_by_ss_id(ss_id):
-    return """SELECT
-							room_id,
-							STRING_AGG(DISTINCT CAST(host_id AS varchar), ' JOIN ') AS host_id,
-							STRING_AGG(DISTINCT name, ' JOIN ') AS name,
-							STRING_AGG(DISTINCT property_type, ' JOIN ') AS property_type,
-							STRING_AGG(DISTINCT room_type, ' JOIN ') AS room_type,
-							AVG(price) AS price,
-							AVG(minstay) AS minstay,
-							AVG(reviews) AS reviews,
-							AVG(avg_rating) AS avg_rating,
-							AVG(accommodates) AS accommodates,
-							AVG(bedrooms) AS bedrooms,
-							AVG(bathrooms) AS bathrooms,
-							STRING_AGG(DISTINCT bathroom, ' JOIN ') AS bathrooms,
-							MAX(latitude) AS latitude,
-							MAX(longitude) AS longitude,
-							STRING_AGG(DISTINCT extra_host_languages, 'JOIN ') AS extra_host_languages,
-							AVG(CAST(is_superhost AS int)) AS is_superhost,
-							STRING_AGG(DISTINCT comodities, ' JOIN ') AS comodities,
-							location.location_id,
-							location.route,
-							location.sublocality,
-							location.locality,
-							'Airbnb' as platform,
-							case
-									when strpos(STRING_AGG(DISTINCT bathroom, 'JOIN '), 'shared') <> 0
-										then 'shared'
-									when strpos(STRING_AGG(DISTINCT bathroom, 'JOIN '), 'private') <> 0
-										then 'private'
-										else  null
-									end
-							as bathrooms
-						FROM
-							room
-					INNER JOIN location
-					ON location.location_id = room.location_id
-				INNER JOIN survey
-					ON survey.survey_id = room.survey_id
-				WHERE room.survey_id in ( select distinct(survey_id) as survey_id from survey where ss_id = {ss_id} )
-						GROUP BY
-							locality, location.sublocality, location.route, location.location_id, room_id, bathroom
-				""".format(ss_id=ss_id)
-
-
-def deprecated_get_booking_rooms_by_ss_id(ss_id):
-    return """SELECT
-							room_id,
-							STRING_AGG(DISTINCT hotel_name, ' JOIN ') AS name,
-							STRING_AGG(DISTINCT room_name, ' JOIN ') AS hotel_name,
-							STRING_AGG(DISTINCT property_type, ' JOIN ') AS property_type,
-							STRING_AGG(DISTINCT room_type, ' JOIN ') AS room_type,
-							AVG(price) AS price,
-							AVG(reviews) AS reviews,
-							AVG(overall_satisfaction) AS avg_rating,
-							AVG(accommodates) AS accommodates,
-							AVG(bedrooms) AS bedrooms,
-							AVG(bathrooms) AS bathrooms,
-							MAX(latitude) AS latitude,
-							MAX(longitude) AS longitude,
-							STRING_AGG(DISTINCT comodities, ' JOIN ') AS comodities,
-							location.location_id,
-							location.route,
-							location.sublocality,
-							location.locality,
-							'Booking' as platform
-						FROM
-							booking_room as b
-					INNER JOIN location
-					ON location.location_id = b.location_id
-				INNER JOIN survey
-					ON survey.survey_id = b.survey_id
-				WHERE b.survey_id in ( select distinct(survey_id) as survey_id from survey where ss_id = {ss_id} )
-						GROUP BY
-							locality, location.sublocality, location.route, location.location_id, room_id
-				""".format(ss_id=ss_id)
